36085d5512e57993b8c154781e16da79c5d0f4f1
JustBeGabs <justbegabs@gmail.com>
2023-06-04 15:17:11 -0300

    ajustes

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ function myFunction(elem) {
 }
 
 function getRandomInterval(min, max) {
-  return Math.floor(Math.random() * (max - min));
+  return Math.floor(min + Math.random() * (max - min + 1));
 }
 
 function setDiceValue(elem) {
   var x = document.getElementById(elem);
   console.log(elem, x);
   x.innerHTML = getRandomInterval(1, 20);
+}
+
+function onClickChangeZIndex(myDivId) {
+  var myDiv = document.getElementById(myDivId);
+  console.log('oi', myDiv);
+  console.log('antes', myDiv.style.zIndex);
+  myDiv.style.zIndex = myDiv.style.zIndex - 1;
+  console.log('depois', myDiv.style.zIndex);
 }
